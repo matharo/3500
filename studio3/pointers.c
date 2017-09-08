c4a695c7e1a48a0ca976944206007056b994311e
Lorna <lornaxiao@linuxclassroom12.mcs.slu.edu>
2017-09-08 13:12:24 -0500

    add studio 2 and 3

--- studio3/pointers.c
+++ studio3/pointers.c
@@ -1,5 +1,5 @@
-//Your name
-//The date
+//Lorna Xiao
+//Sept 7 2017
 //This program illustrates some uses of pointers in C
 
 #include <stdio.h>
@@ -7,11 +7,38 @@
 char linString[] = "Linux!";
 int  linSize = 6;
 
-char *winString = "Windows!";
+char *winString = "Windows!\0";
+
 int  winSize = 8;
 
 int main ( int argc, char* argv[] ){
 
+	printf("%s\n", linString);
+	printf("%s\n", winString);
+
+	int i;
+	for (i = 0; i < linSize; i++)
+	{ printf("%c\n", linString[i]); }
+	
+	printf("%c\n", *winString);
+	printf("%c\n", *(winString + 1));
+	printf("%c\n", *(winString + 2));
+	printf("%c\npointer arithmetic\n", *(winString + 3));
+
+	for (i = 0; i < 8; i++)
+	{ printf("%c\n",*(winString + i)); }
+
+
+//number9 help
+	for (i = 0; i < 8; i++)
+        { printf("%s\n",*(winString+i)); }
+
+	*linString = 'M';
+	printf("%s\n",linString);
+	
+//number11 help
+	*winString = 'T';
+	printf("%s\n",*winString);
 
 	return 0;
 }
