6bf4eda72552958c0131ffbc2370c2a164851616
Lorna <lornaxiao@hopper.mcs.slu.edu>
2017-09-13 18:22:19 -0500

    add studio5

---
+++ studio5/fork.c
@@ -0,0 +1,28 @@
+//Lorna Xiao and Vyshnavee Reddlapalli
+//September 13, 2017
+//Fork and Exec studio5
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+	pid_t pid = fork();
+	pid_t child_PID = getpid();
+	pid_t parent_PID = getpid();
+
+	char* child_argv[] = {"ls","-l",NULL};
+	char* child_prog = child_argv[0];
+
+	if (pid==0){
+                sleep(3);
+                printf("I am the child %d\n",child_PID);
+
+                execvp(child_prog,child_argv);
+        }
+	else{
+		waitpid(child_PID,NULL,0);
+		printf("I am the parent %d\n",parent_PID);
+	}
+	return 0;
+}
