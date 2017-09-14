82e819942ca2a62b0cab69b5583a00994fdeee26
Lorna <lornaxiao@hopper.mcs.slu.edu>
2017-09-13 22:27:23 -0500

    update all

---
+++ studio6/parse.c
@@ -0,0 +1,10 @@
+//Lorna Xiao and Vyshnavee Reddlapalli
+//September 13 2017
+//Parsing String Input studio6
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(int argc, char* argv[]){
+	
