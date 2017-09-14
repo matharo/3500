da17793b85f511511d7b8ca0b8c5d9c6aec8ef04
Lorna <lornaxiao@hopper.mcs.slu.edu>
2017-09-13 22:27:02 -0500

    update

--- studio4/open.c
+++ studio4/open.c
@@ -8,17 +8,27 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+#define buffer_size 200
 int main(int argc, char* argv[])
 {
 	int fd;
 	int textLen;
+	int text; 
+	char buffer[buffer_size];
 
 	fd = open(argv[1],O_RDONLY);
-	struct stat buf;
-	fstat(fd,&buf);
-	textLen = buf.st_size;
-	numBytes = read(fd,argv[1],textLen);
-	write(STDOUT_FILENO,fd,numBytes);
 
+	if (fd==-1) {
+		perror("error opening file\n");
+		printf("errno = %d\n",errno);
+		exit(1);
+	}
+
+	while((text = read(fd,buffer,buffer_size)!=0)){
+		write(STDOUT_FILENO,buffer,buffer_size);
+		
+	}
+	close (fd);
 	return 0;
 }
