a4f833a08e0a15669fae7dfb8c39c05c8bd55a31
Lorna <lornaxiao@linuxclassroom12.mcs.slu.edu>
2017-09-15 13:32:57 -0500

    update ans

--- studio6/parse.c
+++ studio6/parse.c
@@ -6,5 +6,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+#define string_size 1024
 int main(int argc, char* argv[]){
-	
+	char string[string_size];
+	printf("Enter input: ");
+	fgets(string,string_size,stdin);	
+	printf("Your input is: %s\n",string);
+	return 0;
+}
