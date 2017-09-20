b847bfb56e522525d50fc3fc08a6261609a066c0
Lorna <lornaxiao@hopper.mcs.slu.edu>
2017-09-20 17:07:01 -0500

    add studio6

--- studio6/parse.c
+++ studio6/parse.c
@@ -4,13 +4,43 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-
+#include <string.h>
 #define string_size 1024
 int main(int argc, char* argv[]){
 	char string[string_size];
+	char* input;
+	char* res;
+
+	int max_args = 15;
+	int max_argv_size = max_args + 2; //one for argv[0], one for null terminator
+	char* cmd;
+	char* my_argv[max_argv_size];
+
 	printf("Enter input: ");
-	fgets(string,string_size,stdin);	
-	printf("Your input is: %s\n",string);
+	input = fgets(string,string_size,stdin);
+	string[(strlen(string)-1)]=0;
+
+	printf("Your input is: %s\n",string);	
+
+	int i;
+	cmd = strtok(input, " ");
+	my_argv[0] = cmd;
+	printf("First token: %s\n", cmd);
+	for (i = 1; i < max_args; i++)
+	{
+		res = strtok(NULL, " ");
+		if (res == NULL) {
+			my_argv[i] = '\0';
+		}
+		else {
+			my_argv[i] = res;
+			printf("Other tokens: %s\n",res);
+		}
+	}
+	
+	for (i = 0; i < sizeof(my_argv); i++)
+	{
+		execvp(cmd,my_argv);
+	}
 	return 0;
 }
