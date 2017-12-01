46b8b28f3cd5cb56358b10ce0f105865c31704d6
Lorna <lornaxiao@hopper.mcs.slu.edu>
2017-11-30 18:40:46 -0600

    add studio 24

---
+++ studio24/client2.c
@@ -0,0 +1,50 @@
+//Lorna Xiao
+//Studio 24
+//client.c
+//Received source code and help from https://vcansimplify.wordpress.com/2013/03/14/c-socket-tutorial-echo-server/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netdb.h>
+#include <stdio.h>
+#include<string.h>
+ 
+int main(int argc,char **argv)
+{
+    int sockfd,n;
+    char sendline[100];
+    char recvline[100];
+    struct sockaddr_in servaddr;
+ 
+    sockfd=socket(AF_LOCAL,SOCK_STREAM,0);
+    bzero(&servaddr,sizeof servaddr);
+ 
+    servaddr.sin_family=AF_LOCAL;
+    servaddr.sin_port=htons(22000);
+ 
+    //set ip address to...
+    //servaddr needs to be int
+    inet_pton(AF_LOCAL,"127.0.0.1",&(servaddr.sin_addr));
+ 
+    //connect device whose address and port number is in servaddr
+    connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+ 
+    //client repeats forever
+    while(1)
+    {
+	//clear
+        bzero( sendline, 100);
+        bzero( recvline, 100);
+	//reading string from stdin in sendline
+        fgets(sendline,100,stdin); /*stdin = 0 , for standard input */
+ 
+	//write sendline in sockfd
+        write(sockfd,sendline,strlen(sendline)+1);
+	//read from sockfd in screvline
+        read(sockfd,recvline,100);
+	//display recvline
+        printf("%s",recvline);
+    }
+ 
+}
